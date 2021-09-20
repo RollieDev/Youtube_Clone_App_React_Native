fc60a0772bdbbe1848990a91ba5dfd08039e05f2
RollieDev <rolandoaniel@gmail.com>
2021-09-20 14:40:03 -0700

    Refactored environment

---
+++ YoutubeCloneApp/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,10 @@
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from 'react'
+
+import Colors from '../constants/Colors'
+import useColorScheme from "../hooks/useColorScheme";
+import TabOneScreen from "../screens/TabOneScreen";
+import TabTwoScreen from "../screens/TabTwoScreen";
+// import [BottomtabParamList, TaboneParamList, TabTwoParamList} from '../types';
---
+++ YoutubeCloneApp/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+
+    return (
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
