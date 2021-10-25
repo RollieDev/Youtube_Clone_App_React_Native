23759ee4f9886ac53cc2de5f3c45c5adddf8d3a6
RollieDev <rolandoaniel@gmail.com>
2021-10-24 23:53:55 -0700

    Completed VideoPlayer screen and functionality

--- YoutubeCloneApp/components/VideoComment/index.tsx
+++ YoutubeCloneApp/components/VideoComment/index.tsx
@@ -16,7 +16,7 @@ interface VideoCommentProps {
 const VideoComment = ({ comment }: VideoCommentProps) => {
     return (
         <View style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 10 }}>
-            <Image style={{ width: 35, height: 35, borderRadius: 20 }} source={{ uri: 'https://yt3.ggpht.com/ytc/AKedOLSbEMQPyNa2c4Qrnis38l-hw4s6ROM4PCAYAQ_DgkQ=s88-c-k-c0x00ffffff-no-rj' }} />
+            <Image style={{ width: 35, height: 35, borderRadius: 20 }} source={{ uri: 'https://yt3.ggpht.com/ytc/AKedOLR14vjVBR6TQ5p05QHfZyH0IywWFDeH0MZ2CaT1=s88-c-k-c0x00ffffff-no-rj' }} />
             <Text style={{ color: 'white', marginLeft: 10 }}>Thanks everyone!</Text>
         </View>
     )
